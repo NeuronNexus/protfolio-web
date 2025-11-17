5ac9cdbc01543b8147a6f3c3053df869e3be8183
NeuronNexus <milindmaula@gmail.com>
2025-11-17 19:48:46 +0530

    Animation Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation'; // <-- ADDED THIS IMPORT
 
 // Layout Components
 import Navbar from './components/layout/Navbar';
@@ -21,12 +22,36 @@ import { skills } from './data/skills';
 export default function App() {
   const navItems = ['Home', 'About', 'Education', 'Skills', 'Projects', 'Contact'];
 
+  // --- ANIMATED TAGLINE ---
+  // We create the animation component here
+  const animatedTagline = (
+    <TypeAnimation
+      sequence={[
+        'Full Stack Developer',
+        1000, // wait 1s
+        'ML Enthusiast',
+        1000, // wait 1s
+        'DevOps Engineer',
+        1000, // wait 1s
+        'Software Generalist with Full Stack Expertise & a Data Science Core',
+        3000, // wait 3s before repeating
+      ]}
+      wrapper="span"
+      speed={50}
+      repeat={Infinity}
+    />
+  );
+  // --- END OF ANIMATED TAGLINE ---
+
   return (
     <div className="min-h-screen bg-white">
       <Navbar initials={config.personal.initials} navItems={navItems} />
       
       <main>
-        <Hero name={config.personal.name} tagline={config.personal.tagline} />
+        {/* --- MODIFIED THIS LINE --- */}
+        <Hero name={config.personal.name} tagline={animatedTagline} />
+        {/* --- END OF MODIFICATION --- */}
+        
         <About bio={config.personal.bio} />
         <Education educationData={education} />
         <Skills skillsData={skills} />
